a324fd1ec2386a77e47ef0e6e8257430677fe35f
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-30 22:37:47 -0300

    exibindo produto selecionado no modal

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -12,6 +12,7 @@ export class ProductsComponent {
   selectedProduct: any;
   modalProduct!: boolean;
   produtos: ProductInterface[] = [];
+  groupedProducts: any[] = [];
 
   constructor(private productService: ProductService) { }
 
